046a973bc6364c5217cf9efcf8c1dc06b99d9af4
AmiraAdellL <amiraadell.aa.aa@gmail.com>
2023-08-10 18:19:24 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Start
+ * Description: C program that prints line with function printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+		
+}
