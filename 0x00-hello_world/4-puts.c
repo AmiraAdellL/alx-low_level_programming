4f600d4b416011d4daa9eef9d912e38577f42fc1
AmiraAdellL <amiraadell.aa.aa@gmail.com>
2023-08-10 17:45:23 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main
+ * Description: Program C that uses puts function to print a line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
