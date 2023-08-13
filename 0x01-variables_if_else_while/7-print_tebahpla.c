3fbbb32fd83849eae608269e98645f45555f8fb2
AmiraAdellL <amiraadell.aa.aa@gmail.com>
2023-08-13 14:18:08 -0700

    Reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,9 +4,10 @@
  * Description: a program that prints the lowercase alphabet in reverse
  * Retunr: (0) Success
  */
+
 int main(void)
 {
-	char Letter;
+	char Letter = 'z';
 
 	for (Letter = 'z'; Letter <= 'a'; Letter++)
 	{
