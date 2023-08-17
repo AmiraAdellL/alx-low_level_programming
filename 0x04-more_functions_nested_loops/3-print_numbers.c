d561f148f4996bca7e973f089c54e06e34958bc6
AmiraAdellL <amiraadell.aa.aa@gmail.com>
2023-08-17 15:55:29 -0700

    Print Number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,10 @@ void print_numbers(void)
 {
 	int number;
 
-	for (number = 0; number < 10; number++)
+	for (number = 0; number <= 9; number++)
 	{
 		_putchar(number + '0');
 	}
 	_putchar('\n');
+
 }
