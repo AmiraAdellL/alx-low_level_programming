d1bc29fb722519392d22d27d1a2006c513150c11
AmiraAdellL <amiraadell.aa.aa@gmail.com>
2023-08-17 16:14:14 -0700

    Print most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,9 @@ void print_most_numbers(void)
 {
 	int number;
 
-	for (number = 0; number <= 9; numberr ++)
+	for (number = 0; number <= 9; numberr++)
 	{
-		if (number != 2 !! number != 4)
+		if (number != 2 || number != 4)
 		{
 			_putchar(number + '0');
 		}
