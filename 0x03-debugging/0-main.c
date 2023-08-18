ebe59a6edd127628036abad31e32ef8d1facbb3c
AmiraAdellL <amiraadell.aa.aa@gmail.com>
2023-08-17 17:26:46 -0700

    Multipale Main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - test function prints the integer positive or negative
+ * Return: (0) Success
+*/
+int main(void)
+{
+	int number = 0;
+
+	positive_or_negative(number);
+	return (0);
+}
