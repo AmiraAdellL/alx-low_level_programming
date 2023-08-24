4629760524a8a2b7b6c08d862f00a11f50b6d721
AmiraAdellL <amiraadell.aa.aa@gmail.com>
2023-08-24 16:04:38 -0700

    Strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - a function that compares two strings.
+ * @s1: The first string
+ * @s2: The second string
+ * Return: Result
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int Result = 0;
+
+	while (i >= 0)
+	{
+		result = s1[i] - s2[i];
+		if (result != 0 || (s1[i] == '\0' && s2[i] == '\0'))
+		{
+			break;
+		}
+		i++;
+	}
+	return (result);
+}
